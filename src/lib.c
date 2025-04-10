5c8706301b32f5aefd032448d2dcb23f6e2cb465
Stephen Molloy <sdmolloy@gmail.com>
2025-04-10 23:12:56 +0200

    Nicer plot with better vert axis

--- src/lib.c
+++ src/lib.c
@@ -5,7 +5,7 @@
 #include "lib.h"
 
 #define GRAPH_MARK 'X'
-#define VERT_PTS 20
+#define VERT_PTS 15
 
 void plot_hist(double *data, size_t num_pts) {
     double min_val = min(data, num_pts);
@@ -18,22 +18,20 @@ void plot_hist(double *data, size_t num_pts) {
         lims[i] = low + i * step;
     lims[VERT_PTS] = low + VERT_PTS*step;
 
-    for (int i=VERT_PTS; i>=0; i--) {
-        if (i==VERT_PTS) {
-            printf("%+0.3e ^", lims[i]);
-        } else if (i==0) {
-            printf("%+0.3e -", lims[i]);
-        } else {
+    for (int i=VERT_PTS; i>0; i--) {
+        if (i==VERT_PTS)
+            printf("%+0.3e ^", lims[i]-step/2.0);
+        else if (i==1)
+            printf("%+0.3e -", lims[i]-step/2.0);
+        else
             printf("           |");
-        }
+
         for (size_t j=0; j<num_pts; j++) {
-            if (i == 0 && j!=(num_pts-1))
-                putchar('-');
-            else if (i == 0)
-                putchar('>');
-            else if (lims[i] > data[j] && data[j] >= lims[i-1])
+            if (lims[i] > data[j] && data[j] >= lims[i-1])
                 putchar(GRAPH_MARK);
-            else putchar(' ');
+            else if (i!=1) putchar(' ');
+            else if (j!=(num_pts-1) )putchar('-');
+            else putchar('>');
         }
         putchar('\n');
     }
